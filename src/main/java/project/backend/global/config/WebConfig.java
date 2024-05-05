5d135dc0f1531ec373999fbbda93f90db0638fbf
gabang2 <offbeat1020@naver.com>
2024-05-05 18:15:23 +0900

    Feat: Cors 정책 허용

--- src/main/java/project/backend/global/config/WebConfig.java
+++ src/main/java/project/backend/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package project.backend.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,9 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://www.ticats.site", "https://ticats.site")
-                .allowedMethods("*")
-                .allowCredentials(true) // 쿠키 정책 허용
-                .maxAge(3600);
+                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*");
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/docs/");
+    }
+}
 }
