98a4750141ae248524447cce8632d67a2941a2b2
gabang2 <offbeat1020@naver.com>
2024-05-05 18:01:38 +0900

    Feat: allow-arigin

--- src/main/java/project/backend/global/config/WebConfig.java
+++ src/main/java/project/backend/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowCredentials(true) // 쿠키 정책 허용
                 .maxAge(3600);
