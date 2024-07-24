5c6affeb476d832f215e927da87358562e5cd7e2
gabang2 <offbeat1020@naver.com>
2024-07-24 23:07:21 +0900

    Feat: 이미지 ocr 추가

--- src/main/java/project/backend/domain/ticket/controller/TicketController.java
+++ src/main/java/project/backend/domain/ticket/controller/TicketController.java
@@ -4,9 +4,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import project.backend.domain.culturalevent.entity.CulturalEvent;
 import project.backend.domain.member.entity.Member;
 import project.backend.domain.ticket.dto.TicketCreateDto;
@@ -41,5 +43,15 @@ public class TicketController {
     public ResponseEntity retrieve(@Positive @PathVariable Long id) {
         Ticket ticket = ticketService.getTicket(id);
         return ResponseEntity.status(HttpStatus.OK).body(ticketMapper.ticketToTicketRetrieveDto(ticket));
+    }
+
+    @ApiOperation(value = "티켓 정보 추출(ocr)")
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping(value = "/extract-info", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity extractInfo(
+            @RequestPart(value = "file") MultipartFile file
+    ) {
+        ResponseEntity<String> response = ticketService.extractInfoFromImage(file);
+        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
     }
 }
